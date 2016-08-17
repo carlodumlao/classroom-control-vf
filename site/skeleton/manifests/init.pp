f0df4d9dc7764e49129fcfff8782f31527b2ec98
carlodumlao <carlo.dumlao@gmail.com>
2016-08-17 08:46:53 +0800

    modified due to missing comma

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -7,9 +7,9 @@ class skeleton {
   }
   file { '/etc/skel/.bashrc':
     ensure  =>  file,
-    owner   =>  'root'
-    group   =>  'root'
-    mode    =>  '0644'
+    owner   =>  'root',
+    group   =>  'root',
+    mode    =>  '0644',
     source  =>  'puppet:///modules/skeleton/bashrc',
   }
 }
