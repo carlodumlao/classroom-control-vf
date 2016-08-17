e4215f4676737b59bd573d4506a19e593c876091
carlodumlao <carlo.dumlao@gmail.com>
2016-08-17 09:06:40 +0800

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -10,7 +10,7 @@ class skeleton {
     owner   =>  'root',
     group   =>  'root',
     mode    =>  '0644',
-    source  =>  'puppet:///modules/skel/bashrc',
+    source  =>  'puppet:///modules/skeleton/bashrc',
   }
 }
     
