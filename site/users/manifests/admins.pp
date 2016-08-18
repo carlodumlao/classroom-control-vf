9fdee44bf056102dacb10e4597e2938625a427ff
carlodumlao <carlo.dumlao@gmail.com>
2016-08-18 12:24:37 +0800

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,11 +1,7 @@
-class user::admins  {
+class users::admins  {
   users::managed_user {'joe':}
   users::managed_user {'alice':}
-    group => 'staff',
-  }
   users::managed_user {'chen':}
-    group => 'staff',
-  }
   group {'staff:
     ensure  => present,
   }
