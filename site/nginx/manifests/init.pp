a594e1b6133eb8e5fdfa608f16ddb5147c63148a
carlodumlao <carlo.dumlao@gmail.com>
2016-08-19 09:53:40 +0800

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,33 +1,3 @@
-class nginx {
-  File {
-    owner =>  'root',
-    group =>  'root',
-    mode  =>  '0644',
+ class {'nginx':
+    root  => '/var/www/html',
   }
-  package { 'nginx':
-    ensure => present,
-  }
-  file {['/var/www', '/etc/nginx/conf.d']:
-    ensure  =>  directory,
-  }
-  file { '/var/www/index.html':
-    ensure  => file,
-    source  => 'puppet:///modules/nginx/index.html',
-  }
-  file { '/etc/nginx/nginx.conf':
-    ensure  => file,
-    source  => 'puppet:///modules/nginx/nginx.conf',
-    require => Package['nginx'],
-    notify  => Service['nginx'],
-  }
-  file {  '/etc/nginx/conf.d/default.conf':
-    ensure => file,
-    source => 'puppet:///modules/nginx/default.conf',
-    require => Package['nginx'],
-    notify  => Service['nginx'],
-  }
-  service { 'nginx':
-    ensure => running,
-    enable => true,
-  }
-}
